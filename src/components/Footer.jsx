3b6d3b7b177e3aa37e3c21d024666f2d90abacb5
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-20 23:09:30 +0500

    feat: created Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { FaGithubSquare, FaTelegram } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="max-w-[1200px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12">
+        <div className="">
+          <h3 className="text-2xl text-gray-200 font-semibold">M.Muzaffarov</h3>
+          <div className="flex flex-row gap-6 text-gray-400 text-4xl mt-2">
+            <a href="https://github.com/FulgurVividus" target="_blank">
+              <FaGithubSquare />
+            </a>
+            <a href="https://t.me/MurodjonMuzaffarov" target="_blank">
+              <FaTelegram />
+            </a>
+          </div>
+        </div>
+
+        <p className="text-gray-400">©2024 M.Muzaffarov</p>
+      </div>
+    </>
+  );
 };
 
 export default Footer;
