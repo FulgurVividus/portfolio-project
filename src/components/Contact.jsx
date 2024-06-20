fc7ff112d094ebe962f07a03afd984b00954a971
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-21 01:13:44 +0500

    feat: done Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,14 @@
-import React, { useRef } from "react";
-import { FaTelegram } from "react-icons/fa";
-import { AiFillGithub } from "react-icons/ai";
-import emailjs from "@emailjs/browser";
+import React, { useState } from "react";
 
 const Contact = () => {
-  const form = useRef();
-
-  const sendEmail = function (e) {
-    e.preventDefault();
-
-    emailjs
-      .send("service_hr0s3kq", "template_d20gv0i", form.current, {
-        publicKey: "lG-nLHPX7LSZNEYC6",
-      })
-      .then(alert(`Email Sent :)`))
-      .catch((error) => alert(error));
-  };
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   return (
     <>
       <div
-        className="max-w-[1000px] mx-auto px-6 grid md:grid-cols-2 place-items-center"
+        className="max-w-[1000px] mx-auto mt-24 px-6 grid md:grid-cols-2 place-items-center"
         id="contact"
       >
         <div>
@@ -29,24 +17,24 @@ const Contact = () => {
               Let&apos;s <span>connect</span>
             </h3>
             <p className="text-justify leading-7">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores
-              explicabo quibusdam ad illum, iste dicta a eius neque vel cum
-              voluptatibus illo. Sit tempora fugit aliquid laboriosam ad iusto
-              quia, magni optio porro maiores quam deserunt consectetur beatae
-              numquam eius! Magni soluta quaerat consectetur reiciendis.
-              Aspernatur aperiam numquam dolore odit?
+              I am always eager to explore new opportunities and collaborate
+              with professionals in the tech industry. If you have a project in
+              mind, are interested in discussing potential roles, or have any
+              questions, please don&apos;t hesitate to reach out via the form. I
+              am committed to responding promptly and look forward to the
+              possibility of working together.
             </p>
           </div>
 
           <div className="flex mt-10 items-center gap-7">
-            <div className="bg-[#171717]/40 p-5 rounded-lg">
+            <div className="p-5 rounded-lg">
               <h3 className="text-2xl md:text-4xl font-semibold text-gray-200">
                 5 <span>+</span>
               </h3>
               <p className="text-xs md:text-base text-gray-200">Projects</p>
             </div>
 
-            <div className="bg-[#171717]/40 p-5 rounded-lg">
+            <div className="p-5 rounded-lg">
               <h3 className="text-2xl md:text-4xl font-semibold text-gray-200">
                 -
               </h3>
@@ -55,7 +43,7 @@ const Contact = () => {
               </p>
             </div>
 
-            <div className="bg-[#171717]/40 p-5 rounded-lg">
+            <div className="p-5 rounded-lg">
               <h3 className="text-2xl md:text-4xl font-semibold text-gray-200">
                 ∞ <span>+</span>
               </h3>
@@ -67,34 +55,30 @@ const Contact = () => {
         </div>
 
         <form
+          action="https://getform.io/f/paqgrgoa"
+          method="POST"
           className="max-w-6xl p-5 md:p-12"
           id="form"
-          ref={form}
-          onSubmit={sendEmail}
         >
           <input
             type="text"
             id="name"
             placeholder="Your name ..."
-            name="name"
+            name="user_name"
             className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4"
             required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
           <input
             type="email"
             id="email"
             placeholder="Your email ..."
-            name="email"
+            name="user_email"
             className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4"
             required
-          />
-          <input
-            type="subject"
-            id="subject"
-            placeholder="The subject ..."
-            name="subject"
-            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4"
-            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <textarea
             type="textarea"
@@ -102,8 +86,11 @@ const Contact = () => {
             rows="4"
             id="textarea"
             placeholder="Your message ..."
+            name="message"
             className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 resize-none"
             required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
           <button
             type="submit"
