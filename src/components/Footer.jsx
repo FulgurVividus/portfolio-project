ce3659a5fa54a45f653e2265b9a438def9f3b120
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2024-06-21 13:37:26 +0500

    styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { FaGithubSquare, FaTelegram } from "react-icons/fa";
 const Footer = () => {
   return (
     <>
-      <div className="max-w-[1200px] mx-auto flex justify-between p-6 md:p-20 text-sm md:text-lg mt-12">
+      <div className="max-w-[1200px] mx-auto gap-6 flex justify-between p-6 md:gap-0 md:p-20 text-sm md:text-lg mt-12">
         <div className="">
           <h3 className="text-2xl text-gray-200 font-semibold">M.Muzaffarov</h3>
           <div className="flex flex-row gap-6 text-gray-400 text-4xl mt-2">
